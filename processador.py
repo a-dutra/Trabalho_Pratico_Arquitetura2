9507d6804d08a74259876eb6ada2279965176d08
Ana <anadutragit@gmail.com>
2025-11-28 19:04:10 -0300

    Quase finalizado

--- processador.py
+++ processador.py
@@ -162,19 +162,6 @@ class Processador:
                             break
             #se não encontrou em caches, busca na RAM e carrega como M
             print("Buscando bloco na memória principal...")
-           
-            self.cache.carregar_linha(
-                self.memoria_principal.buscar_bloco(endereco),
-                endereco,
-                Estado.M
-            )
-
-            # --- TESTE AQUI ---
+            self.cache.carregar_linha(self.memoria_principal.buscar_bloco(endereco), endereco, Estado.M)
             linha_local = self.cache.procurar_linha(endereco)
-            if linha_local is None:
-                raise RuntimeError("Erro: carregar_linha NÃO armazenou o bloco na cache!")
-
-            # se passou do teste, pode escrever
-            linha_local.dados[endereco % self.cache.tamanho_linha] = dado
-            linha_local.estado = Estado.M
-
+            linha_local.dados[endereco % self.cache.tamanho_linha] = dado # escrita do dado dentro do bloco da cache
